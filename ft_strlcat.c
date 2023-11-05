b4c187fb4d85cb1af00453bab4b087c87ecff0e8
Merretby <125912627+Merretby@users.noreply.github.com>
2023-11-05 18:32:46 +0100

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -19,13 +19,13 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	size_t	ln_s;
 	size_t	ln_d;
 
-	if (dest == NULL)
+	if (size == 0)
 		return (0);
 	i = 0;
 	j = ft_strlen(dest);
 	ln_s = ft_strlen(src);
 	ln_d = ft_strlen(dest);
-	if ((size <= ln_d) || (size == 0))
+	if (size <= ln_d)
 		return (size + ln_s);
 	while (src[i] && j < size - 1)
 	{
